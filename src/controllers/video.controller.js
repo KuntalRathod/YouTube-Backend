beae8a6294342f15ab81ac7a8ab9b3cfcfa3681a
KuntalRathod <nature.kr70@gmail.com>
2024-03-01 13:41:18 +0530

    Add getVideoById function to video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -148,14 +148,38 @@ const updateVideo = asyncHandler(async (req, res) => {
     )
 })
 
+const getVideoById = asyncHandler(async (req, res) => {
+  //TODO: get video by id
+
+  const { videoId } = req.params
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "this video id is not valid !!")
+  }
+
+  const video = await Video.findById({
+    _id : videoId,
+  })
+
+  if (!video) {
+    throw new ApiError(404,"Video is not found!!")
+  }
+
+  //return res
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        video,
+        "video fetched successfully!"
+      )
+    )
+})
+
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
   //TODO: get all videos based on query, sort, pagination
-})
-
-const getVideoById = asyncHandler(async (req, res) => {
-  const { videoId } = req.params
-  //TODO: get video by id
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
