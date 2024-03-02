c45d3fdcf23d107d44172ebf26ebb7bacaf841cb
KuntalRathod <nature.kr70@gmail.com>
2024-03-02 12:26:48 +0530

    Refactor video controller and add deleteVideo functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,7 +5,6 @@ import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { deleteOnCloudinary, uploadOnCloudinary } from "../utils/Cloudinary.js"
-import { useId } from "react/cjs/react.production.min.js"
 
 const publishAVideo = asyncHandler(async (req, res) => {
   // TODO: get video, upload to cloudinary, create video
@@ -174,15 +173,25 @@ const getVideoById = asyncHandler(async (req, res) => {
 
 const getAllVideos = asyncHandler(async (req, res) => {
   //TODO: get all videos based on query, sort, pagination
+
   const {
+    // If 'page' exists in req.query, its value is assigned to the 'page' variable. If it doesn't exist, 'page' is assigned a default value of 1.
     page = 1,
+
+    // If 'limit' exists in req.query, its value is assigned to the 'limit' variable. If it doesn't exist, 'limit' is assigned a default value of 10.
     limit = 10,
+
+    // If 'query' exists in req.query, its value is assigned to the 'query' variable. If it doesn't exist, 'query' is assigned a default value of `/^video/`.
     query = `/^video/`,
+
+    // If 'sortBy' exists in req.query, its value is assigned to the 'sortBy' variable. If it doesn't exist, 'sortBy' is assigned a default value of "createdAt".
     sortBy = "createdAt",
-    sortType = 1,
+
+    // If 'sortType' exists in req.query, its value is assigned to the 'sortType' variable. If it doesn't exist, 'sortType' is assigned a default value of 1.
+    sortType = req.query.sortType ? parseInt(req.query.sortType) : 1,
+    // 'userId' is assigned the value of 'req.user._id'. Note that there's no default value here, so if 'req.user._id' is undefined, 'userId' will also be undefined.
     userId = req.user._id,
   } = req.query
-
   //find user in db
   const user = await User.findById({
     _id: userId,
@@ -198,7 +207,7 @@ const getAllVideos = asyncHandler(async (req, res) => {
       // The $match stage filters the documents to pass only the documents that match the specified condition(s) to the next pipeline stage.
       $match: {
         // This line matches videos where the videoOwner field is equal to the userId
-        videoOwner: new mongoose.Types.ObjectId(useId),
+        videoOwner: new mongoose.Types.ObjectId(userId),
         // This line matches videos where the title or description fields contain the query string
         $or: [
           {
@@ -231,23 +240,75 @@ const getAllVideos = asyncHandler(async (req, res) => {
       // If the aggregation and pagination are successful, a 200 status code and the results are returned
       return res
         .status(201)
-        .json(
-          new ApiResponse(
-            200,
-            result,
-            "Fetched all videos successfully!!"
-          ) 
-        )
+        .json(new ApiResponse(200, result, "Fetched all videos successfully!!"))
+    })
+    .catch((error) => {
+      // If there is an error during the aggregation or pagination, the error is logged and thrown
+      console.log("getting error while fetching all videos", error)
+      throw error
     })
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
+  // Extract the videoId from the request parameters.
+  // Check if the videoId is a valid MongoDB ObjectId. If it's not, throw an error.
+  // Find the video in the database using the videoId. If the video is not found, throw an error.
+  // Check if the owner of the video matches the user making the request. If they don't match, throw an error.
+  // If the video has a videoFile, delete it from Cloudinary (a cloud-based image and video management service).
+  // If the video has a thumbnail, delete it from Cloudinary.
+  // Delete the video from the database. If the deletion is unsuccessful, throw an error.
+  // Return a response with a status code of 201 and a message indicating that the video and files were deleted successfully.
+
   const { videoId } = req.params
-  //TODO: delete video
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "this video id is not valid")
+  }
+
+  const video = await Video.findById({
+    _id: videoId,
+  })
+
+  if (!video) {
+    throw new ApiError(404, "Video or file not found!!")
+  }
+
+  if (video.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(
+      400,
+      "you do not have to permission to delete video or file!!"
+    )
+  }
+
+  if (video?.videoFile) {
+    await deleteOnCloudinary(video?.videoFile?.public_id, "video")
+  }
+  if (video?.thumbnail) {
+    deleteOnCloudinary(video?.thumbnail?.public_id,"any")
+  }
+
+
+  const deleteResponse = await Video.findByIdAndDelete(videoId)
+
+  if (!deleteResponse) {
+    throw new ApiError(400,"Something went wrong while deleting file or video!!")
+  }
+
+  //return res
+  return res
+    .status(201)
+    .json(new ApiResponse(
+      200,
+      deleteResponse,
+      "video or file delete successfully!!"
+    ))
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params
+  
+
+
 })
 
 export {
