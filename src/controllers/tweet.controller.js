31f6eb004553b017d46b9f9c676851f8bec835b6
KuntalRathod <nature.kr70@gmail.com>
2024-03-08 23:28:26 +0530

    Add console.log to print tweetId

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -113,6 +113,7 @@ const updateTweet = asyncHandler(async (req, res) => {
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
   const { tweetId } = req.params
+  console.log(tweetId);
 
   if (!isValidObjectId(tweetId)) {
     throw new ApiError(400, "this tweet id is not valid !!")
