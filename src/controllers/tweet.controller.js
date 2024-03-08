7230ee1749530318d07179b38c51cf4614ad6c3b
KuntalRathod <nature.kr70@gmail.com>
2024-03-08 23:29:38 +0530

    Remove console.log statement and handle case when tweet is not found

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -119,7 +119,6 @@ const deleteTweet = asyncHandler(async (req, res) => {
   }
 
   const tweet = await Tweet.findById(tweetId)
-  console.log(tweet);
 
   if (!tweet) {
     throw new ApiError(404, "this tweet is not found")
