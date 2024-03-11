e44d7a471144a774a60a7520c437f384c31b5359
KuntalRathod <nature.kr70@gmail.com>
2024-03-11 16:47:11 +0530

    Update comment routes to include separate endpoints for video and tweet comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,17 +1,29 @@
 import { Router } from "express"
 import {
-  addComment,
-  deleteComment,
+  addCommentToVideo,
+  addCommentToTweet,
   getVideoComments,
-  updateComment,
+  getTweetComments,
+  updateCommentToVideo,
+  updateCommentToTweet,
+  deleteCommentToVideo,
+  deleteCommentToTweet,
 } from "../controllers/comment.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
 router.use(verifyJWT) // Apply verifyJWT middleware to all routes in this file
+router.route("/video/:videoId").get(getVideoComments).post(addCommentToVideo)
+router.route("/tweet/:tweetId").get(getTweetComments).post(addCommentToTweet)
 
-router.route("/:videoId").get(getVideoComments).post(addComment)
-router.route("/c/:commentId").delete(deleteComment).patch(updateComment)
+router
+  .route("/video/c/:commentId")
+  .delete(deleteCommentToVideo)
+  .patch(updateCommentToVideo)
+router
+  .route("/tweet/c/:commentId")
+  .delete(deleteCommentToTweet)
+  .patch(updateCommentToTweet)
 
 export default router
