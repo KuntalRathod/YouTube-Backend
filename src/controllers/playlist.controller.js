1b74bf757f7a8e30e7f735b3c531a42b025f6b3a
KuntalRathod <nature.kr70@gmail.com>
2024-03-17 12:32:26 +0530

    Fix incorrect property name in removeVideoFromPlaylist function

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -218,7 +218,7 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   }
 
   //if video exists but not in playlists
-  if (!playlist.video.includes(videoId)) {
+  if (!playlist.videos.includes(videoId)) {
     throw new ApiError(400, "video exists but not exists in playlist!!")
   }
 
