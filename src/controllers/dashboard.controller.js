c9da5ab3e4cd513b5fe8febb42de53c1269ed0b7
KuntalRathod <nature.kr70@gmail.com>
2024-03-25 17:12:02 +0530

    getChannelStats func added

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -12,15 +12,98 @@ const getChannelStats = asyncHandler(async (req, res) => {
   const allLikes = await Like.aggregate([
     {
       $match: {
-        likedBy: new mongoose.Types.ObjectId(req.user._id),
+        likedBy: new mongoose.Types.ObjectId(req.user._id), // Matching likes by the current user
       },
     },
-    {},
+    {
+      $group: {
+        _id: null,
+        totalVideoLikes: {
+          $sum: {
+            $cond: [
+              { $ifNull: ["$video", false] },
+              1, //not null then add 1
+              0, //else 0
+            ],
+          },
+        },
+        totalTweetLikes: {
+          $sum: {
+            $cond: [{ $ifNull: ["$tweet", false] }, 1, 0], // If tweet field is not null, add 1
+          },
+        },
+        totalCommentLikes: {
+          $sum: {
+            $cond: [{ $ifNull: ["$comment", false] }, 1, 0], // If comment field is not null, add 1
+          },
+        },
+      },
+    },
   ])
+
+  // Total subscribers of the channel
+  const allsubscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new mongoose.Types.ObjectId(req.user._id), // Matching subscriptions by the current user's channel
+      },
+    },
+    {
+      $count: "subscribers", // Counting the number of subscribers
+    },
+  ])
+
+  // Total videos uploaded by the user
+  const allVideo = await Video.aggregate([
+    {
+      $match: {
+        videoOwner: new mongoose.Types.ObjectId(req.user._id), // Matching videos by the current user
+      },
+    },
+    {
+      $count: "Videos", // Counting the number of videos
+    },
+  ])
+
+  // Total views on videos uploaded by the user
+  const allViews = await Video.aggregate([
+    {
+      $match: {
+        videoOwner: new mongoose.Types.ObjectId(req.user._id), // Matching videos by the current user
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        allVideosViews: {
+          $sum: "$views", // Summing up the views of all videos
+        },
+      },
+    },
+  ])
+
+  // Constructing the final stats object
+  const stats = {
+    Subscribers: allsubscribers[0].subscribers,
+    totalVideos: allVideo[0].Videos,
+    totalVideoViews: allViews[0].allVideosViews,
+    totalVideoLikes: allLikes[0].totalVideoLikes,
+    totalTweetLikes: allLikes[0].totalTweetLikes,
+    totalCommentLikes: allLikes[0].totalCommentLikes,
+  }
+
+  //return res
+  return res
+    .status(201)
+    .json(new ApiResponse(200, stats, "fetching channel stats successfully!!"))
 })
 
-const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+const getChannelVideos = asyncHandler(async (req, res) => {
+
+
+
+
 })
 
 export { getChannelStats, getChannelVideos }
