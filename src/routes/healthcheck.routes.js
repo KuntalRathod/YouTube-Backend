79cd285a73fcaa8206f22a887ed5348da8d1f3bb
KuntalRathod <nature.kr70@gmail.com>
2024-05-29 02:34:13 +0530

    chore: Update import statement for healthcheck controller in healthcheck.routes.js

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { healthcheck } from "../controllers/healthcheck.controller"
+import { healthcheck } from "../controllers/healthcheck.controller.js"
 
 const router = Router()
 
